926eb8e1ca97efc8644e6ea5ba9f662b55f947d9
turanbul <turanbul@gmail.com>
2015-09-21 22:59:53 +0200

    Update after the certification

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-#1. Merges the training and the test sets to create one data set.
+#1. Merge the training and the test sets to create one data set.
 
 ##Importing column names
 features=t(read.table(file="~/features.txt",header=FALSE))[2,]
